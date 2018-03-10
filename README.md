95c08feb20f532cd52a68e966c66dfd8a873fa2b
Thomas Seaman <thomasseaman@dhcp-v096-225.mobile.uci.edu>
2018-03-10 14:50:15 -0800

    group push

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 # V3 - Gavin - 3/6/18 - 4:00PM
 # V4 - Cameron - 3/7/18 - 8:35PM
 # V5 - Group - 3/10/18 - 11:30AM
+# V6 - Group - 3/10/18 - 2:50PM
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,14 +3,30 @@ var idArray = []
 var latLngArray = []
 var locNameArray = []
 var breweryIdArray = []
-var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A1','B1','C1','D1','E1','F1','G1','H1','I1','J1','K','L1','M1','N1','O1','P1','Q1','R1','S1','T1','U1','V1','W1','X1','Y1','Z1']
 var labelIndex = 0
+
+//on enter within "formUserState" fire onclick associated with the submitBtn
+$("#formUserState").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $('#submitBtn').click();
+    }
+});
 
 // On Submit Button Click
 $('#submitBtn').on('click', function () {
 
+    // Empty any previous data from brewery card container
+    $('#breweryCard').empty()
+    $('#map').empty()
+    var latLngArray = []
+    var latLngArray = []
+
+    // Grab user city and state input, and convert into URI code
     var userCity = $('#formUserCity').val()
     var userState = $('#formUserState').val()
+    var userCity = encodeURIComponent(userCity.trim())
+    var userState = encodeURIComponent(userState.trim())
 
     // ajax request for user input data
     $.ajax({
@@ -18,12 +34,11 @@ $('#submitBtn').on('click', function () {
         method: "GET"
     }).then(function (response) {
 
-
+        // move to top of map
         $('html, body').animate({
             scrollTop: ($('#map').offset().top)
-        }, 50)
+        }, 500)
 
-console.log(response.data[0])
         // Loop through api data and send relevant information to global variables
         for (i = 0; i < response.data.length; i++) {
             var lat = response.data[i].latitude
@@ -51,28 +66,31 @@ console.log(response.data[0])
 
             //if then to handle blank icons
             if (typeof brewImages != 'undefined') {
-                var brewIcon = response.data[i].brewery.images.icon
-                console.log("found undefined")
+                var brewIcon = response.data[i].brewery.images.large
             }
             else {
-                var brewIcon = "brewPlaceholder.png"
+                var brewIcon = "assets/images/brewPlaceholder.png"
 
             }
 
-            // var pic = (response.data[i].brewery.images.icon)
-
-            // console.log('image link =' + pic)
+            // if then to handle undefined description
+            if (typeof description != 'undefined') {
+                var description = (response.data[i].brewery.description)
+            }
+            else {
+                var description = "This brewery has no available description"
+            }
 
             //Append search results to the page - dynamic jquery using string interpolation
             $('#breweryCard').append(`
-                        <div class="uk-card uk-card-default uk-width-1-2@m">
+                        <div class="uk-card uk-card-default uk-width-1-1">
                             <div class="uk-card-header">
                                 <div class="uk-grid-small uk-flex-middle" uk-grid>
                                     <div class="uk-width-auto">
-                                        <img class="uk-border-circle" width="40" height="40" src="${brewIcon}">
+                                        <img class="uk-border-circle" width="100" height="100" src="${brewIcon}">
                                     </div>
                                     <div class="uk-width-expand">
-                                        <h3 id="card-${breweryId}" class="uk-card-title uk-margin-remove-bottom">${alphabet[i]}. ${breweryName}</h3>
+                                        <h3 id="card-${breweryId}" class="uk-card-title uk-margin-remove-bottom">${breweryName}</h3>
                                     </div>
                                 </div>
                             </div>
@@ -90,15 +108,6 @@ console.log(response.data[0])
                         `)
         }
 
-        
-
-        // Calculation for center of lat/long
-        // for (m = 0; m < beerMap.length; m++) {
-        //     var latLngHolder = 0
-        //     var latLngHolder = latLngHolder + beerMap[m]
-        //     console.log(latLngHolder)
-        // }
-
         // Initialize map with latLngArray data
         initMap(latLngArray)
 
@@ -106,12 +115,8 @@ console.log(response.data[0])
         console.log(err)
     })
 
-
-
 // Create Map Function
 function initMap(beerMap) {
-
-    console.log(breweryIdArray)
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
@@ -121,34 +126,16 @@ function initMap(beerMap) {
     for (i = 0; i < beerMap.length; i++) {
         var position = beerMap[i]
         var breweryId = breweryIdArray[i]
-        //console.log('breweryIdArray', breweryIdArray);
-        //console.log('breweryIdArray[' + i + ']', breweryIdArray[i]);
-        // var locationInfowindow = new google.maps.InfoWindow({
-        //     content: 'Test content',
-        //   })
         var marker = new google.maps.Marker({
             animation: google.maps.Animation.DROP,
             position: position,
             map: map,
             title: locNameArray[i],
             label: alphabet[i],
-            id: "marker" + i,
-            class: "marker",
-            breweryId: breweryIdArray[i], // doesn't do anything
         })
-
-        console.log('marker', marker);
 
         // On Marker Click Event
         google.maps.event.addListener(marker, 'click', function(e) {
-            console.log('e', e);
-            //console.log(JSON.stringify(e.target));
-            // map.setZoom(18);
-            // map.setCenter(marker.getPosition())
-            // var markerPosition = marker.getPosition()
-            console.log('lat', e.latLng.lat());
-            console.log('lng', e.latLng.lng());
-            console.log('latLngArray', latLngArray);
 
             var lat = e.latLng.lat();
             var lng = e.latLng.lng();
@@ -162,18 +149,15 @@ function initMap(beerMap) {
                 //Math.round is rounding to the 3rd decimal place
                 if (Math.round(arrLat * 1000) / 1000 === Math.round(lat * 1000) / 1000
                     && Math.round(arrLng * 1000) / 1000 === Math.round(lng * 1000) / 1000) {
-                    console.log('matched on index', i);
+                    // console.log('matched on index', i);
                     breweryIndex = i;//assign brewery index a value of index i
                     break;
                 }
             }
             
-            //onsole.log('marker', marker)
-            //console.log('breweryId', marker.breweryId)
             var scrollBreweryId = breweryIdArray[breweryIndex];
 
             var scroll = $("#card-"+scrollBreweryId).offset().top;
-            //console.log('scroll', scroll);
 
             $('html, body').animate({
                 scrollTop: scroll
@@ -183,27 +167,4 @@ function initMap(beerMap) {
 
 }
 
-})
-
-
-
-// populate container with test divs
-// for (i = 0; i < 20; i++) {
-//     $('.container').append('<div class="row" id="row' + i + '">').append('<div class="col-md-12"').append('<p id="p' + i + '">THIS IS TEST MATERIAL # ' + i + '</p>')
-// }
-
-// // on test btn click
-// $('#testBtn').on('click', function () {
-// // // scroll to third row
-// $('html, body').animate({
-//     scrollTop: ($('#row2').offset().top)
-// },500);
-// })
-
-
-
-$("#testBtn").on('click', function () {
-    $('html, body').animate({
-        scrollTop: ($('#row3').offset().top)
-    }, 300)
 })
